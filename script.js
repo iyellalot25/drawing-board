9e5f9262a496bee34efbfc1f6a2bf9aa4839c536
Srijan Ghosh <ghoshsrijan.future@gmail.com>
2025-12-28 22:05:14 +0530

    Add tool selection and rectangle drawing to canvas app
    Introduced tool selection functionality by assigning IDs and classes to tool buttons in index.html and handling tool selection in script.js. Added support for drawing rectangles based on mouse events, and set up global variables to track the selected tool and mouse positions.

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const toolBtns = document.querySelectorAll(".tool");
 
+//global var with default val
+let prevMouseX, prevMouseY;
 let isDrawing = false;
+let selectedTool = "brush";
 let brushWidth = 5;
 
 window.addEventListener('load', () => {
@@ -9,18 +13,37 @@ window.addEventListener('load', () => {
   canvas.height = canvas.offsetHeight;
 });
 
-canvas.addEventListener('mousedown', () => {
+//startDraw
+canvas.addEventListener('mousedown', (e) => {
   isDrawing = true;
+  prevMouseX = e.offsetX;
+  prevMouseY = e.offsetY;
   ctx.beginPath(); //Everytime mousedown new path not continue from prev path
   ctx.lineWidth = brushWidth;
 });
 
+//drawing
 canvas.addEventListener('mousemove', (e) => {
   if(!isDrawing) return; //immediately return dont draw
-  ctx.lineTo(e.offsetX, e.offsetY); //creating line according to mouse coordinates
-  ctx.stroke();  //drawing/fillng line with color
+  if(selectedTool === 'brush') {
+    ctx.lineTo(e.offsetX, e.offsetY); //creating line according to mouse coordinates
+    ctx.stroke();  //drawing/fillng line with color
+  } else if(selectedTool === 'rectangle') {
+    const width = prevMouseX - e.offsetX;
+    const height =  prevMouseY - e.offsetY;
+    ctx.strokeRect(e.offsetX, e.offsetY, width, height);
+  }
 });
 
+//stopDraw
 canvas.addEventListener('mouseup', () => {
   isDrawing = false;
 });
+
+toolBtns.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    document.querySelector('.options .active').classList.remove('active'); //removing active from current active btn before adding it to clicked btn
+    btn.classList.add('active');
+    selectedTool = btn.id;
+  });
+})
