e77129848c855541955ee1a13a8f08ba51b164d7
Srijan Ghosh <ghoshsrijan.future@gmail.com>
2025-12-28 19:55:05 +0530

    Add basic drawing functionality to canvas
    Implemented mouse event handlers to enable drawing on the canvas using the mouse. Set up brush width and ensured drawing starts and stops with mouse actions.

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+let isDrawing = false;
+let brushWidth = 5;
+
+window.addEventListener('load', () => {
+  canvas.width = canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight;
+});
+
+canvas.addEventListener('mousedown', () => {
+  isDrawing = true;
+  ctx.beginPath(); //Everytime mousedown new path not continue from prev path
+  ctx.lineWidth = brushWidth;
+});
+
+canvas.addEventListener('mousemove', (e) => {
+  if(!isDrawing) return; //immediately return dont draw
+  ctx.lineTo(e.offsetX, e.offsetY); //creating line according to mouse coordinates
+  ctx.stroke();  //drawing/fillng line with color
+});
+
+canvas.addEventListener('mouseup', () => {
+  isDrawing = false;
+});
