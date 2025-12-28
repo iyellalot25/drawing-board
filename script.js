1432b284eab4388da1c6443a00e25aba4d116967
Srijan Ghosh <ghoshsrijan.future@gmail.com>
2025-12-28 22:54:29 +0530

    Add circle drawing tool and fill option
    Introduced a circle tool for drawing circles on the canvas and added support for filling shapes using the fill color checkbox. Snapshot logic was implemented to prevent dragging artifacts during drawing.

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
 const toolBtns = document.querySelectorAll(".tool");
+const fillColor = document.getElementById('fill-color');
+const ctx = canvas.getContext("2d");
 
 //global var with default val
-let prevMouseX, prevMouseY;
+let prevMouseX, prevMouseY, snapshot;
 let isDrawing = false;
 let selectedTool = "brush";
 let brushWidth = 5;
@@ -20,18 +21,28 @@ canvas.addEventListener('mousedown', (e) => {
   prevMouseY = e.offsetY;
   ctx.beginPath(); //Everytime mousedown new path not continue from prev path
   ctx.lineWidth = brushWidth;
+  //copying canvas data and passing as snapshot value this avoids the dragging effect
+  snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
 });
 
 //drawing
 canvas.addEventListener('mousemove', (e) => {
   if(!isDrawing) return; //immediately return dont draw
+  ctx.putImageData(snapshot, 0, 0); //adding copied snapshot to current canvas
+
   if(selectedTool === 'brush') {
     ctx.lineTo(e.offsetX, e.offsetY); //creating line according to mouse coordinates
     ctx.stroke();  //drawing/fillng line with color
   } else if(selectedTool === 'rectangle') {
     const width = prevMouseX - e.offsetX;
     const height =  prevMouseY - e.offsetY;
-    ctx.strokeRect(e.offsetX, e.offsetY, width, height);
+    fillColor.checked ? ctx.fillRect(e.offsetX, e.offsetY, width, height) : ctx.strokeRect(e.offsetX, e.offsetY, width, height);
+  } else if(selectedTool === 'circle') {
+    ctx.beginPath(); //resetting path so that repeated circles dont form
+    //calculating radius according to mouseptr
+    let radius = Math.sqrt(Math.pow((prevMouseX - e.offsetX), 2) + Math.pow((prevMouseY - e.offsetY), 2));
+    ctx.arc(prevMouseX, prevMouseY, radius, 0, 2*Math.PI);
+    fillColor.checked ? ctx.fill() : ctx.stroke();
   }
 });
 
@@ -46,4 +57,4 @@ toolBtns.forEach((btn) => {
     btn.classList.add('active');
     selectedTool = btn.id;
   });
-})
+});
